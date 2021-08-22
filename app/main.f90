68d2cef7b2dff60723b0687914de1a08f3f1398a
Ned Richards <edwardlrichards@gmail.com>
2021-08-22 12:40:00 -0700

    fixed form version of pade coeffs

--- app/main.f90
+++ app/main.f90
@@ -1,6 +1,17 @@
 program main
   use fpm_test, only: say_hello
+  !use oper,     only: g
+  use pade_coeffs, only: epade
+
   implicit none
 
-  call say_hello()
+  complex*16 pd1(20),pd2(20)
+  real*8 k0, dr
+
+  k0 = 6 / 1500.
+  dr = 200.
+
+  call epade(20_8,6_8,1_8,2_8,k0,dr,pd1,pd2)
+  print *, pd1(:6)
+
 end program main
